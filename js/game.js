1c6db6b1544e00b12158d9b7ba3c252991ca9c77
IvianskiyA <IvianskiyA@mail.ru>
2019-12-24 01:24:04 +0300

    Ver 1

--- js/game.js
+++ js/game.js
@@ -3,50 +3,77 @@ const maxHits = 10;
 
 let hits = 0;
 let firstHitTime = 0;
+let missHit = 0;
+
+let timerId = 0; 
 
 function round() {
-  // FIXME: надо бы убрать "target" прежде чем искать новый
+  // D FIXME: надо бы убрать "target" прежде чем искать новый
   $(".target").text('');
   $(".target").removeClass('target');
   let divSelector = randomDivId();
   $(divSelector).addClass("target");
-  // TODO: помечать target текущим номером
+  // D TODO: помечать target текущим номером
   $(divSelector).text(hits+1)
-  // FIXME: тут надо определять при первом клике firstHitTime
-
+  // D FIXME: тут надо определять при первом клике firstHitTime
+  // передвинули в reload
   if (hits === maxHits) {
     endGame();
   }
 }
 
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
+  // D FIXME: спрятать игровое поле сначала
   $(".target").text('');
   $(".target").removeClass('target');
   let totalPlayedMillis = getTimestamp() - firstHitTime;
-  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(4);
   $("#total-time-played").text(totalPlayedSeconds);
-
   $("#win-message").removeClass("d-none");
+  $(".game-field").addClass("d-none");
+  clearInterval(timerId);
+  $("#timer-message").addClass("d-none");
 }
 
 function handleClick(event) {
-  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  // D FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  $(".game-field").removeClass('miss');
   if ($(event.target).hasClass("target")) {
     hits = hits + 1;
     round();
   }
-  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+  else if ($(".game-field").hasClass("target"))
+  { 
+    $(event.target).addClass("miss");
+    maxHit = missHit + 1;
+  }
+  // D TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+}
+function reload()
+{
+  firstHitTime = getTimestamp();
+  hits = 0;
+  missHit = 0;
+  $("#win-message").addClass("d-none");
+  $(".game-field").removeClass("d-none");
+  $("#button-reload").text('Играть заново');
+  round();
+  timerId = setInterval(function(){$("#time-left").text((getTimestamp()-firstHitTime) / 1000)}, 10)
+  $("#timer-message").removeClass("d-none");
+  $("#button-start").addClass('d-none');
+  $("#button-reload").removeClass('d-none');
 }
 
 function init() {
-  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
-  round();
-
+  //  TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
+  
   $(".game-field").click(handleClick);
-  $("#button-reload").click(function() {
-    location.reload();
+  $("#button-start").click(reload);
+  $("#button-reload").click(function(){
+        location.reload();
   });
+  $("#win-message").addClass("d-none"); // убрать надпись с результатом
+
 }
 
 $(document).ready(init);
